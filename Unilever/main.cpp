fe2dc78268b53c9f58cac46c1e644eb6681c7848
Kirill Kotov <kkaprof@gmail.com>
2015-07-06 11:31:16 +0300

    qDebug to file

--- Unilever/main.cpp
+++ Unilever/main.cpp
@@ -1,9 +1,44 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+#include <QFile>
+#include <QDateTime>
+#include <QDebug>
+#include <QTextStream>
+
+
+
+void myMessageOutput(QtMsgType type, const QMessageLogContext& context, const QString& msg) {
+    QFile fMessFile(qApp->applicationDirPath() + "/unilever.log");
+    if(!fMessFile.open(QIODevice::Append | QIODevice::Text)){
+        return;
+    }
+    QString sCurrDateTime = "[" + QDateTime::currentDateTime().toString("dd.MM.yyyy hh:mm:ss.zzz") + "]";
+    QTextStream tsTextStream(&fMessFile);
+    switch(type){
+    case QtDebugMsg:
+        tsTextStream << QString("UnileverLogDebug%1: %2\n").arg(sCurrDateTime).arg(msg);
+        break;
+    case QtWarningMsg:
+        tsTextStream << QString("UnileverLogWarning%1: %2\n").arg(sCurrDateTime).arg(msg);
+        break;
+    case QtCriticalMsg:
+        tsTextStream << QString("UnileverLogCritical%1: %2\n").arg(sCurrDateTime).arg(msg);
+        break;
+    case QtFatalMsg:
+        tsTextStream << QString("UnileverLogFatal%1: %2\n").arg(sCurrDateTime).arg(msg);
+        abort();
+    }
+    tsTextStream.flush();
+    fMessFile.flush();
+    fMessFile.close();
+}
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    qInstallMessageHandler(myMessageOutput);
 
     MainWindow w;
     w.showMaximized();
